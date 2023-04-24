66b8c46f896db4a76ff086491bc8ac792a2a970e
Teo-Medesi <teo.medesi@gmail.com>
2023-04-24 12:15:05 +0200

    mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import "../assets/styles/navbar.scss"
+import menu from "../assets/svgs/menu.svg"
+import email from "../assets/svgs/email.svg"
+
 
 const Navbar = ({onContact}) => {
     return (
         <motion.nav initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 2}}}>
-            <p className='logo'>Teo Međeši</p>
+            <div className='logo-div'>
+                <p className='logo'>Teo Međeši</p>
+                <img src={menu} alt="menu icon"/>
+            </div>
             <ul className='links'>
                 <li>Home</li>
                 <li>About</li>
@@ -13,7 +19,10 @@ const Navbar = ({onContact}) => {
                 <li>Blog</li>
             </ul>
 
-            <motion.button onClick={onContact} whileTap={{scale: 0.8}}>Contact me</motion.button>
+            <div className="contact-div">
+                <motion.button onClick={onContact} whileTap={{scale: 0.8}}>Contact me</motion.button>
+                <img src={email} alt="email icon" />
+            </div>
         </motion.nav>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -121,105 +121,96 @@ const Home = () => {
   }
 
   return (
-      <div className='home'>
-      <Navbar onContact={handleContactClick} />
-      <header>
-        <div className='header-text'>
-          <h5>{headerText1}<span className="h5-span">{headerText1Span}</span></h5>
-          <h1>{headerText2}<span className="h1-span">{headerText2Span}</span>{headerText3}</h1>
-          <p>{headerText4}</p>
-        </div>
-
-        
-        
-      </header>
-
-      <section className="skills-section">
-        <div class="shape-divider">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-          </svg>
-        </div>
-        <div class="shape-divider-2">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-          </svg>
-        </div>
-        <div class="shape-divider-3">
-          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
-          </svg>
-        </div>
-
-        <div ref={skillsScope} className="section-panel">
-          <motion.div initial={{opacity: 0}} className='text'>
-            <h3>Services I offer</h3>
-            <p className="sm">Are you ready to make your website a game-changer? Let's build a website that's not only visually stunning, but also highly functional and optimized for success. </p>
-          </motion.div>
-
-
-          <ul>
-            <motion.div initial={{opacity: 0}} className='row'>
-              {skills1.map((skill, index) => <Skill skill={skill.name} src={skill.src} index={index} />)}
-            </motion.div>
-
-            <motion.div initial={{opacity: 0}} className='row row-2'>
-              {skills2.map((skill, index) => <Skill skill={skill.name} src={skill.src} index={index} />)}
-            </motion.div>
-          </ul>
-        </div>
-      </section>
-      <section className="about-me-container">
-        <motion.article ref={aboutScope} className="about-me">
-          <div className="text-col">
-            <motion.h3 initial={{opacity: 0}} className="about-header">About me</motion.h3>
-            <motion.p initial={{opacity: 0}} className="about-paragraph">
-              I have always been a <span>problem solver</span> and had a keen interest in <span>building things</span>. The satisfaction I feel after a long and intense project is immeasurable. My love for <span>programming</span> and all things <span>IT</span> runs deep, and I am always eager to expand my knowledge and skills. I take great pride in <span>staying up-to-date</span> with the latest trends and techniques.
-              <br />
-              <br />
-              <span>Integrity is paramount</span>. When working with clients or future employers, I prioritize <span>listening</span> to their needs and tailoring my approach accordingly. My goal is to deliver a product that <span>exceeds their expectations</span> and provides them with tangible value.
-
-
-            </motion.p>
+      <>
+        <Navbar onContact={handleContactClick} />
+        <div className='home'>
+        <header>
+          <div className='header-text'>
+            <h5>{headerText1}<span className="h5-span">{headerText1Span}</span></h5>
+            <h1>{headerText2}<span className="h1-span">{headerText2Span}</span>{headerText3}</h1>
+            <p>{headerText4}</p>
           </div>
-          <motion.a initial={{opacity: 0}} href="https://github.com/Teo-Medesi" className="github-button"><img src={GITHUB} alt="github" /></motion.a>
-        </motion.article>
-      </section>
-
-      <section ref={contactScope} className="contact-me-container">
-        <article className="contact-me">
-        <motion.div initial={{opacity: 0}} className="text">
-            <h6>Contact Me!</h6>
-            <p className="xsm">The first step towards success is just a message away. Contact me using the form below, and let's start building something great together!</p>
-          </motion.div>
-          <motion.div initial={{opacity: 0}} className="input-field">
-            <label htmlFor="name">Name</label>
-            <input onChange={event => setName(event.target.value)} placeholder="John Doe" type="text" />
-          </motion.div>
-          <motion.div initial={{opacity: 0}} className="input-field">
-            <label htmlFor="email">Email</label>
-            <input onChange={event => setEmail(event.target.value)} placeholder="example123@gmail.com" type="email" />
-          </motion.div>
-          <motion.div initial={{opacity: 0}} className="input-field">
-            <label htmlFor="message">Message</label>
-            <textarea onChange={event => setMessage(event.target.value)} placeholder="Enter your message here..." type="text" ></textarea>
-          </motion.div>
-
-          <motion.button onClick={handleSubmit} initial={{opacity: 0}} className="submit-button">{isSubmitted ? "Thank you for your feedback!" : "Submit"}</motion.button>
-        </article>
-      </section>
-
-      <footer>
-        <p className="xsm">Images by <a href="https://pixabay.com/users/amusic11-21978593/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6806813">Andrea Music</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6806813">Pixabay</a></p>
-      </footer>
-
-    </div>
+        
+        
+        </header>
+        <section className="skills-section">
+          <div className="shape-divider">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+            </svg>
+          </div>
+          <div className="shape-divider-2">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+            </svg>
+          </div>
+          <div className="shape-divider-3">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+            </svg>
+          </div>
+          <div ref={skillsScope} className="section-panel">
+            <motion.div initial={{opacity: 0}} className='text'>
+              <h3>Services I offer</h3>
+              <p className="sm">Are you ready to make your website a game-changer? Let's build a website that's not only visually stunning, but also highly functional and optimized for success. </p>
+            </motion.div>
+            <ul>
+              <motion.div initial={{opacity: 0}} className='row'>
+                {skills1.map((skill, index) => <Skill skill={skill.name} src={skill.src} key={index} />)}
+              </motion.div>
+              <motion.div initial={{opacity: 0}} className='row row-2'>
+                {skills2.map((skill, index) => <Skill skill={skill.name} src={skill.src} key={index} />)}
+              </motion.div>
+            </ul>
+          </div>
+        </section>
+        <section className="about-me-container">
+          <motion.article ref={aboutScope} className="about-me">
+            <div className="text-col">
+              <motion.h3 initial={{opacity: 0}} className="about-header">About me</motion.h3>
+              <motion.p initial={{opacity: 0}} className="about-paragraph">
+                I have always been a <span>problem solver</span> and had a keen interest in <span>building things</span>. The satisfaction I feel after a long and intense project is immeasurable. My love for <span>programming</span> and all things <span>IT</span> runs deep, and I am always eager to expand my knowledge and skills. I take great pride in <span>staying up-to-date</span> with the latest trends and techniques.
+                <br />
+                <br />
+                <span>Integrity is paramount</span>. When working with clients or future employers, I prioritize <span>listening</span> to their needs and tailoring my approach accordingly. My goal is to deliver a product that <span>exceeds their expectations</span> and provides them with tangible value.
+              </motion.p>
+            </div>
+            <motion.a initial={{opacity: 0}} href="https://github.com/Teo-Medesi" className="github-button"><img src={GITHUB} alt="github" /></motion.a>
+          </motion.article>
+        </section>
+        <section ref={contactScope} className="contact-me-container">
+          <article className="contact-me">
+          <motion.div initial={{opacity: 0}} className="text">
+              <h6>Contact Me!</h6>
+              <p className="xsm">The first step towards success is just a message away. Contact me using the form below, and let's start building something great together!</p>
+            </motion.div>
+            <motion.div initial={{opacity: 0}} className="input-field">
+              <label htmlFor="name">Name</label>
+              <input onChange={event => setName(event.target.value)} placeholder="John Doe" type="text" />
+            </motion.div>
+            <motion.div initial={{opacity: 0}} className="input-field">
+              <label htmlFor="email">Email</label>
+              <input onChange={event => setEmail(event.target.value)} placeholder="example123@gmail.com" type="email" />
+            </motion.div>
+            <motion.div initial={{opacity: 0}} className="input-field">
+              <label htmlFor="message">Message</label>
+              <textarea onChange={event => setMessage(event.target.value)} placeholder="Enter your message here..." type="text" ></textarea>
+            </motion.div>
+            <motion.button onClick={handleSubmit} initial={{opacity: 0}} className="submit-button">{isSubmitted ? "Thank you for your feedback!" : "Submit"}</motion.button>
+          </article>
+        </section>
+        <footer>
+          <p className="xsm">Images by <a href="https://pixabay.com/users/amusic11-21978593/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6806813">Andrea Music</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6806813">Pixabay</a></p>
+        </footer>
+        
+            </div>
+      </>
   )
 }
 
-const Skill = ({skill, src, index}) => {
+const Skill = ({skill, src}) => {
   return (
-    <div className="skill" key={index}>
+    <div className="skill">
       <img loading="lazy" src={src} alt={skill} />
       <p>{skill}</p>
     </div>
